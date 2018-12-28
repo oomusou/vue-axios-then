bf0047e59aebd102a021e8dcf8e8f6c1b383c281
oomusou <oomusou@gmail.com>
2018-12-28 15:54:14 +0800

    Data in component

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,10 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'func-names': 'off',
+    'space-before-function-paren': 'off',
+    'no-return-assign': 'off',
+    'no-param-reassign': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
